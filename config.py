2a4bae55e64c3e7f40943e4a3b764b09db19d4ad
avelok <mraurelius@yandex.ru>
2021-08-30 22:33:01 +0300

    Add dotnet-env

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), 'config.env')
+load_dotenv(dotenv_path)
+
 settings = {
-    'token': 'YOUR_TOKEN',
-    'bot': 'BOT_NAME',
-    'id': 00000,    # BOT ID INTEGER,
+    'token': os.environ.get("TOKEN"),
+    'bot': os.environ.get("BOT_NAME"),
+    'id': os.environ.get("ID"),
     'command_prefix': '*'
 }
